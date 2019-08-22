79fe0d6fb41bfb0a87ee7ad92cc80cc54f83cbda
amy-2310 <53995970+amy-2310@users.noreply.github.com>
2019-08-22 09:10:40 +0200

    Add files via upload

---
+++ tests.hs
@@ -0,0 +1,67 @@
+--tests in thesis section 5.4 
+import RandomTreeTest
+tests = do 
+        print_test 50 1 1 10 1
+        print_test 50 2 1 10 1
+        print_test 50 3 1 10 1
+        print_test 50 4 1 10 1
+        print_test 50 5 1 10 1
+
+        print_test 50 1 2 10 1
+        print_test 50 2 2 10 1
+        print_test 50 3 2 10 1
+        print_test 50 4 2 10 1
+        print_test 50 5 2 10 1
+
+        print_test 50 1 3 10 1
+        print_test 50 2 3 10 1
+        print_test 50 3 3 10 1
+        print_test 50 4 3 10 1
+        print_test 50 5 3 10 1
+
+        print_test 50 1 4 10 1
+        print_test 50 2 4 10 1
+        print_test 50 3 4 10 1
+        print_test 50 4 4 10 1
+        print_test 50 5 4 10 1
+
+        print_test 50 1 5 10 1
+        print_test 50 2 5 10 1
+        print_test 50 3 5 10 1
+        print_test 50 4 5 10 1
+        print_test 50 5 5 10 1
+
+
+        print_test 50 1 6 10 1
+        print_test 50 2 6 10 1
+        print_test 50 3 6 10 1
+        print_test 50 4 6 10 1
+        print_test 50 5 6 10 1
+
+
+        print_test 50 1 7 10 1
+        print_test 50 2 7 10 1
+        print_test 50 3 7 10 1
+        print_test 50 4 7 10 1
+        print_test 50 5 7 10 1
+
+
+        print_test 50 1 8 10 1
+        print_test 50 2 8 10 1
+        print_test 50 3 8 10 1
+        print_test 50 4 8 10 1
+        print_test 50 5 8 10 1
+
+        print_test 50 1 9 10 1
+        print_test 50 2 9 10 1
+        print_test 50 3 9 10 1
+        print_test 50 4 9 10 1
+        print_test 50 5 9 10 1
+
+        print_test 50 1 10 10 1
+        print_test 50 2 10 10 1
+        print_test 50 3 10 10 1
+        print_test 50 4 10 10 1
+        print_test 50 5 10 10 1
+
+
